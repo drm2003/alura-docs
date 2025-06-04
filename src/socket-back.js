8f03e1184afa6770af7a116a37437692ebaa2677
Daniel Monteiro <daniel.ucb@gmail.com>
2025-06-03 23:18:00 -0300

    aula 4

--- src/socket-back.js
+++ src/socket-back.js
@@ -3,11 +3,21 @@ import io from "./server.js";
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou!", socket.id);
 
-  socket.on("texto_editor", (texto) => {
+  socket.on("selecionar_documento", (nomeDocumento) => {
+    // console.log(nomeDocumento);
+
+    // criando as salas
+    socket.join(nomeDocumento);
+  });
+
+  socket.on("texto_editor", ({ texto, nomeDocumento }) => {
     // envia para todos os usuários
     // io.emit("texto_editor_clientes", texto);
 
     // faz o envio para todos, com exceção do próprio usuário
-    socket.broadcast.emit("texto_editor_clientes", texto);
+    //socket.broadcast.emit("texto_editor_clientes", texto);
+
+    // emite apenas para a sala
+    socket.to(nomeDocumento).emit ("texto_editor_clientes", texto);
   });
 });
